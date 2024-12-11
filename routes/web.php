a52eefd674cb6f9584174d08a9bfa07943a42e2d
S M Noiem Uddin <smnoiem@gmail.com>
2024-12-11 21:51:49 +0600

    initial vaccine front layout added

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Http\Controllers\Front\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::name('front.')->group(function () {
+
+    Route::get('/', [HomeController::class, 'index'])->name('index');
+
 });
 
 Route::get('/dashboard', function () {
