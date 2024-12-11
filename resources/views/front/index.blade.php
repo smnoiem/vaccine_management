9ea6bb32cd172872df52e04565a5bf352d26a80b
S M Noiem Uddin <smnoiem@gmail.com>
2024-12-12 01:01:06 +0600

    vaccine 'status' button showned if already registered

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -3,10 +3,24 @@
     <h3 style="text-align:center">Welcome to Vaxx</h3>
     <hr>
     <div style="padding:6%;font-size:1.5rem;">
-        <a href="{{ route('front.registration.create') }}">Register</a> <br>
-        <a href="">Vaccine Status</a> <br>
-        <a href="/vaccine-card">Download Vaccine Card</a> <br>
-        <a href="/vaccine-certificate">Download Certificate</a> <br>
+        @if(!auth())
+        <div>
+            <ul>
+                <li><a href="{{ route('login') }}">login</a></li>
+                <li><a href="{{ route('register') }}">register</a></li>
+            </ul>
+        </div>
+        @else
+        <div>
+            @if (auth()->user()->registration)
+            <a href="">Vaccine Status</a> <br>
+            @else
+            <a href="{{ route('front.registration.create') }}">Register</a> <br>
+            @endif
+            <a href="/vaccine-card">Download Vaccine Card</a> <br>
+            <a href="/vaccine-certificate">Download Certificate</a> <br>
+        </div>
+        @endif
     </div>
     
 </x-layout>
