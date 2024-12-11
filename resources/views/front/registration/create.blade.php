15321be4a64cc4c87328225e868f69cfe648e141
S M Noiem Uddin <smnoiem@gmail.com>
2024-12-11 22:26:30 +0600

    added registration form, registration proccess in progress

--- resources/views/front/registration/create.blade.php
+++ resources/views/front/registration/create.blade.php
@@ -1,1 +1,51 @@
-create registration
+<x-layout title="New Registration">
+
+    <h3 style="text-align:center;"> Registration Form </h3>
+
+    <form action="{{ route('front.registration.store') }}" method="post">
+        @csrf
+        <span>NID:</span> 
+        {{-- validation needed. probably an external API? or dummy perhaps --}}
+
+        <input type="text" name="nid" value="{{old('nid')}}"> <br>
+        <span style="color: red">
+        @error('nid')
+        {{$message}}
+        @enderror</span>
+        <br>
+
+        <span>Birthdate:</span>
+        <input type="date" name="dob" id="" value="{{old('dob')}}"> <br>
+        <span style="color: red">
+        @error('dob')
+            {{$message}}
+        @enderror
+        </span>
+        <br>
+
+        <span>Phone Number:</span>
+        <input type="text" name="phone" id="phone" value="{{old('phone')}}"> <br>
+        <span style="color: red">
+        @error('phone')
+            {{$message}}
+        @enderror
+        </span><br> <!-- Verify using OTP -->
+
+        <span>Select Center:</span>
+        <select name="center_id" id="center_id" name="center_id">
+        <option value="1">Dhaka</option>
+        <option value="2" selected>Gopalganj</option>
+        <option value="3">Khulna</option>
+        </select> <br>
+        <span style="color: red">
+        @error('center_id')
+            {{$message}}
+        @enderror
+        </span><br>
+        <!-- No need to have a CRUD for centers, seed the pre-defined center names -->
+
+        <input type="submit" value="Submit">
+        
+    </form>
+
+</x-layout>
