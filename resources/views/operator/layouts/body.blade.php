7136354102258e733f6fd79176229f5e8bf941c3
Noiem <smnoiem@gmail.com>
2024-12-17 20:15:07 +0600

    sidebard move out

--- resources/views/operator/layouts/body.blade.php
+++ resources/views/operator/layouts/body.blade.php
@@ -6,63 +6,7 @@
             <div class="wrapper">
                 
                 @include('operator.layouts.navbar')
-
-                <aside class="main-sidebar sidebar-dark-primary elevation-4">
-                    <div class="dropdown">
-                    <a href="./" class="brand-link text-center">
-                    <large><b>Vaxx - Center</b></large>
-                    </a>
-                    
-                    </div>
-                    <div class="sidebar">
-                    <nav class="mt-2">
-                        <ul class="nav nav-pills nav-sidebar flex-column nav-flat" data-widget="treeview" role="menu" data-accordion="false">
-                        <li class="nav-item dropdown">
-                            <a href="/center" class="nav-link nav-home">
-                            <i class="nav-icon fas fa-tachometer-alt"></i>
-                            <p>
-                                Center Dashboard
-                            </p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link nav-edit_user">
-                            <i class="nav-icon fas fa-users"></i>
-                            <p>
-                                Vaccine Registrations
-                                <i class="right fas fa-angle-left"></i>
-                            </p>
-                            </a>
-                            <ul class="nav nav-treeview">
-                            <li class="nav-item">
-                                <a href="" class="nav-link nav-user_list tree-item">
-                                <i class="fas fa-angle-right nav-icon"></i>
-                                <p>List</p>
-                                </a>
-                            </li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link nav-edit_user">
-                            <i class="nav-icon fas fa-users"></i>
-                            <p>
-                                Vaccine Stock
-                                <i class="right fas fa-angle-left"></i>
-                            </p>
-                            </a>
-                            <ul class="nav nav-treeview">
-                            <li class="nav-item">
-                                <a href="" class="nav-link nav-user_list tree-item">
-                                <i class="fas fa-angle-right nav-icon"></i>
-                                <p>List</p>
-                                </a>
-                            </li>
-                            </ul>
-                        </li>
-                        </ul>
-                    </nav>
-                    </div>
-                </aside>
+                @include('operator.layouts.sidebar')
 
 
 
