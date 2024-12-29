827b0cbd30ec3cde885cbecb7dee8ad1894ba37e
munia77 <mahabuba.akter.munia@g.bracu.ac.bd>
2024-12-29 13:46:08 +0600

    logout msg pop up

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -39,8 +39,10 @@ class AuthenticatedSessionController extends Controller
         Auth::guard('web')->logout();
 
         $request->session()->invalidate();
-
         $request->session()->regenerateToken();
+
+        // Add a success message to the session
+        $request->session()->flash('success', 'You are logged out successfully!');
 
         return redirect('/');
     }
