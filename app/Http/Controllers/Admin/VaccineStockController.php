9d8745274d644b05f6676ec12b8515ec250616c9
Noiem <smnoiem@gmail.com>
2025-01-07 18:04:50 +0600

    vaccine import enlistment store API implemented

--- app/Http/Controllers/Admin/VaccineStockController.php
+++ app/Http/Controllers/Admin/VaccineStockController.php
@@ -11,7 +11,7 @@ class VaccineStockController extends Controller
 {
     public function index(Request $request)
     {
-        $vaccineStocks = VaccineStock::doesntHave('center') ?? [];
+        $vaccineStocks = VaccineStock::doesntHave('center')->get() ?? [];
 
         return view('admin.vaccine-stock.index', ['vaccineStocks' => $vaccineStocks]);
     }
@@ -25,6 +25,29 @@ class VaccineStockController extends Controller
 
     public function store(Request $request)
     {
-        dd($request);
+        $vaccine = Vaccine::findOrFail($request->input('vaccine_id', 0));
+        $quantity = request('quantity', 0);
+
+        $vaccineStock = VaccineStock::where('vaccine_id', $vaccine->id)
+            ->whereNull('center_id')
+            ->first();
+
+        if ($vaccineStock)
+        {
+            $vaccineStock->quantity += $quantity;
+            $vaccineStock = $vaccineStock->update();
+
+        } else {
+            $vaccineStock = VaccineStock::create([
+                'vaccine_id' => $vaccine->id,
+                'center_id' => null,
+                'quantity' => $quantity,
+            ]);
+        }
+
+        if (!$vaccineStock)
+            return response('Updating Failed!', 500);
+        else
+            return 1;
     }
 }
